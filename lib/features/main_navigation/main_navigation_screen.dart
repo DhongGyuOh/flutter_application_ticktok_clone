9d6b5cc04993af0ade27a42eeccaee4ecdffab71
DhongGyuOh <mroh1226@naver.com>
2023-07-10 18:38:03 +0900

    BottomNavBar 버튼 커스텀하기

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -20,7 +21,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       child: Text('Search'),
     ),
     const Center(
-      child: Text('friend'),
+      child: Text('Friend'),
     )
   ];
   late int _selectedIndex = 0;
@@ -33,36 +34,93 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [screens[_selectedIndex]],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.shifting,
-          onTap: (value) => _onTapNavButton(value),
-          currentIndex: _selectedIndex,
-          //selectedItemColor: Theme.of(context).primaryColor,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(FontAwesomeIcons.houseChimney),
-                label: "Home",
-                tooltip: "This is Home Screen",
-                backgroundColor: Colors.amber),
-            BottomNavigationBarItem(
-                icon: Icon(FontAwesomeIcons.video),
-                label: "Video",
-                tooltip: "Funny Video",
-                backgroundColor: Colors.blue),
-            BottomNavigationBarItem(
-                icon: Icon(FontAwesomeIcons.search),
-                label: "Search",
-                tooltip: "Search",
-                backgroundColor: Colors.green),
-            BottomNavigationBarItem(
-                icon: Icon(FontAwesomeIcons.userFriends),
-                label: "Friend",
-                tooltip: "Friend",
-                backgroundColor: Colors.deepPurple),
-          ]),
-    );
+        body: Column(
+          children: [screens[_selectedIndex]],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.black,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              NavTap(
+                title: 'Home',
+                icon: FontAwesomeIcons.houseMedical,
+                isSelected: _selectedIndex == 0,
+                onTap: () => _onTapNavButton(0),
+              ),
+              NavTap(
+                title: 'Home',
+                icon: FontAwesomeIcons.houseMedical,
+                isSelected: _selectedIndex == 1,
+                onTap: () => _onTapNavButton(1),
+              ),
+              NavTap(
+                title: 'Home',
+                icon: FontAwesomeIcons.houseMedical,
+                isSelected: _selectedIndex == 2,
+                onTap: () => _onTapNavButton(2),
+              ),
+              NavTap(
+                title: 'Home',
+                icon: FontAwesomeIcons.houseMedical,
+                isSelected: _selectedIndex == 3,
+                onTap: () => _onTapNavButton(3),
+              )
+            ],
+          ),
+        ));
+  }
+
+  NavigationBar DestinationNavBar() {
+    return NavigationBar(
+        onDestinationSelected: _onTapNavButton,
+        selectedIndex: _selectedIndex,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        destinations: const [
+          NavigationDestination(
+              icon: FaIcon(
+                FontAwesomeIcons.houseMedical,
+                color: Colors.green,
+              ),
+              label: "Home",
+              selectedIcon: FaIcon(FontAwesomeIcons.houseMedicalCircleCheck)),
+          NavigationDestination(
+              icon: FaIcon(FontAwesomeIcons.video), label: "Video"),
+          NavigationDestination(
+              icon: FaIcon(FontAwesomeIcons.searchengin), label: "Search"),
+          NavigationDestination(
+              icon: FaIcon(FontAwesomeIcons.users), label: "Friend"),
+        ]);
+  }
+
+  BottomNavigationBar bottomNavBar() {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        onTap: (value) => _onTapNavButton(value),
+        currentIndex: _selectedIndex,
+        //selectedItemColor: Theme.of(context).primaryColor,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.houseChimney),
+              label: "Home",
+              tooltip: "This is Home Screen",
+              backgroundColor: Colors.amber),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.video),
+              label: "Video",
+              tooltip: "Funny Video",
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.search),
+              label: "Search",
+              tooltip: "Search",
+              backgroundColor: Colors.green),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.userFriends),
+              label: "Friend",
+              tooltip: "Friend",
+              backgroundColor: Colors.deepPurple),
+        ]);
   }
 }
---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NavTap extends StatelessWidget {
+  const NavTap(
+      {super.key,
+      required this.title,
+      required this.icon,
+      required this.isSelected,
+      required this.onTap});
+  final String title;
+  final IconData icon;
+  final bool isSelected;
+  final Function onTap;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          color: Colors.red,
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 300),
+            opacity: isSelected ? 1 : 0.6,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(
+                  icon,
+                  color: Colors.white,
+                ),
+                Text(
+                  title,
+                  style: const TextStyle(color: Colors.white),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
