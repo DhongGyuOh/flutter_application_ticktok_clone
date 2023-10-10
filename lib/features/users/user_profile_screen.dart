a1729550223057679817949bc26a0f4664834c5c
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2023-10-10 11:08:00 +0900

    NestedScrollView 만들기

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_ticktok_clone/constants/gaps.dart';
+import 'package:flutter_application_ticktok_clone/features/users/widgets/persistent_tabbar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -12,158 +13,166 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      animationDuration: const Duration(milliseconds: 100),
-      length: 2,
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            centerTitle: true,
-            title: const Text(
-              "Gyu",
-              style: TextStyle(color: Colors.black),
-            ),
-            actions: [
-              IconButton(
-                  onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.bell)),
-              IconButton(
-                  onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.gear))
-            ],
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                const CircleAvatar(
-                  radius: 50,
-                  backgroundImage: NetworkImage(
-                      "https://yt3.ggpht.com/LOWZ9per6hyhd6swd5KEi_RmgACl6-gpXSUf91zNaY-fPb8jX13syeVo-RKLFqqUxnyqCi4S=s88-c-k-c0x00ffffff-no-rj"),
+    return SafeArea(
+      child: DefaultTabController(
+        animationDuration: const Duration(milliseconds: 100),
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                centerTitle: true,
+                title: const Text(
+                  "Gyu",
+                  style: TextStyle(color: Colors.black),
                 ),
-                Gaps.v10,
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                actions: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: const FaIcon(FontAwesomeIcons.bell)),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const FaIcon(FontAwesomeIcons.gear))
+                ],
+              ),
+              SliverToBoxAdapter(
+                child: Column(
                   children: [
-                    Text("@mroh1226"),
-                    Gaps.h3,
-                    FaIcon(
-                      FontAwesomeIcons.certificate,
-                      size: 15,
-                      color: Colors.blue,
-                    )
-                  ],
-                ),
-                Gaps.v20,
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: SizedBox(
-                    height: 50,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    const CircleAvatar(
+                      radius: 50,
+                      backgroundImage: NetworkImage(
+                          "https://yt3.ggpht.com/LOWZ9per6hyhd6swd5KEi_RmgACl6-gpXSUf91zNaY-fPb8jX13syeVo-RKLFqqUxnyqCi4S=s88-c-k-c0x00ffffff-no-rj"),
+                    ),
+                    Gaps.v10,
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        TextNumber(
-                          numbers: "37",
-                          title: "Following",
-                        ),
-                        VerticalDivider(
-                            width: 30,
-                            thickness: 0.4,
-                            color: Colors.grey,
-                            endIndent: 14,
-                            indent: 14),
-                        TextNumber(
-                          numbers: "10.5M",
-                          title: "Followers",
-                        ),
-                        VerticalDivider(
-                            width: 30,
-                            thickness: 0.4,
-                            color: Colors.grey,
-                            endIndent: 14,
-                            indent: 14),
-                        TextNumber(
-                          numbers: "149K",
-                          title: "Like",
+                        Text("@mroh1226"),
+                        Gaps.h3,
+                        FaIcon(
+                          FontAwesomeIcons.certificate,
+                          size: 15,
+                          color: Colors.blue,
                         )
                       ],
                     ),
-                  ),
-                ),
-                Gaps.v20,
-                FractionallySizedBox(
-                  widthFactor: 0.33,
-                  child: Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(4))),
-                    child: const Text(
-                      'Follow',
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w600),
+                    Gaps.v20,
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: SizedBox(
+                        height: 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            TextNumber(
+                              numbers: "37",
+                              title: "Following",
+                            ),
+                            VerticalDivider(
+                                width: 30,
+                                thickness: 0.4,
+                                color: Colors.grey,
+                                endIndent: 14,
+                                indent: 14),
+                            TextNumber(
+                              numbers: "10.5M",
+                              title: "Followers",
+                            ),
+                            VerticalDivider(
+                                width: 30,
+                                thickness: 0.4,
+                                color: Colors.grey,
+                                endIndent: 14,
+                                indent: 14),
+                            TextNumber(
+                              numbers: "149K",
+                              title: "Like",
+                            )
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Gaps.v5,
-                const Text('64 Telecaster, Jazz Standard Bass, SLG200N'),
-                Gaps.v5,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FaIcon(
-                      FontAwesomeIcons.link,
-                      size: 15,
-                      color: Colors.blue.shade400,
+                    Gaps.v20,
+                    FractionallySizedBox(
+                      widthFactor: 0.33,
+                      child: Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(4))),
+                        child: const Text(
+                          'Follow',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w600),
+                        ),
+                      ),
                     ),
-                    const Text(' www.youtube.com'),
+                    Gaps.v5,
+                    const Text('64 Telecaster, Jazz Standard Bass, SLG200N'),
+                    Gaps.v5,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.link,
+                          size: 15,
+                          color: Colors.blue.shade400,
+                        ),
+                        const Text(' www.youtube.com'),
+                      ],
+                    ),
+                    Gaps.v10,
                   ],
                 ),
-                Gaps.v10,
-                Container(
-                  decoration: const BoxDecoration(
-                      border: Border.symmetric(
-                          horizontal:
-                              BorderSide(width: 0.8, color: Colors.grey))),
-                  child: TabBar(
-                      indicatorSize: TabBarIndicatorSize.tab,
-                      indicatorColor: Theme.of(context).primaryColor,
-                      labelPadding: const EdgeInsets.all(10),
-                      labelColor: Colors.black,
-                      tabs: const [
-                        Icon(Icons.grid_4x4),
-                        FaIcon(FontAwesomeIcons.heart)
-                      ]),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: TabBarView(
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      children: [
-                        GridView.builder(
-                          itemCount: 10,
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                  mainAxisExtent: 300,
-                                  mainAxisSpacing: 5,
-                                  crossAxisSpacing: 5,
-                                  childAspectRatio: 0.2,
-                                  crossAxisCount: 2),
-                          itemBuilder: (context, index) => Container(
-                            child: Container(
-                                alignment: Alignment.center,
-                                color: Colors.deepOrangeAccent.shade100,
-                                child: Text('$index')),
-                          ),
-                        ),
-                        const Center(child: Text('data'))
-                      ]),
-                )
-              ],
-            ),
-          )
-        ],
+              ),
+              SliverPersistentHeader(
+                delegate: PersistentTabbar(),
+                pinned: true,
+              ),
+            ];
+          },
+          body: TabBarView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              children: [
+                GridView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: 12,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            mainAxisExtent: 200,
+                            crossAxisSpacing: 2,
+                            mainAxisSpacing: 2,
+                            childAspectRatio: 9 / 16,
+                            crossAxisCount: 3),
+                    itemBuilder: (context, index) => Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Image.network(
+                              "https://source.unsplash.com/random",
+                              fit: BoxFit.cover,
+                            ),
+                            const Positioned(
+                                bottom: 5,
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.play_arrow_outlined,
+                                      color: Colors.white,
+                                    ),
+                                    Text(
+                                      "1.5K",
+                                      style: TextStyle(color: Colors.white),
+                                    )
+                                  ],
+                                ))
+                          ],
+                        )),
+                const Center(child: Text('data'))
+              ]),
+        ),
       ),
     );
   }
