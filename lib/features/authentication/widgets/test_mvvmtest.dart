8a341649c0c6a8b6d03a181aff5d81f4cfba2693
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2023-11-30 18:03:28 +0900

    Authenication email/password test 구현

--- lib/features/authentication/widgets/test_mvvmtest.dart
+++ lib/features/authentication/widgets/test_mvvmtest.dart
@@ -1,123 +1,224 @@
 import 'dart:async';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-//Model
-class User {
-  User({required this.name, required this.autoLogin});
-  final String name;
-  final bool autoLogin;
+////Firebase///////////////////////////////////////////////////////////////////////
+class AuthRepository {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  //Firebase의 Authentication을 다룰 수 있도록 FirebaseAuth 인스턴스를 생성함
+  User? get user => _firebaseAuth.currentUser; //get 정의하는 방법 중 하나
+  bool get isLoggedIn => user != null;
+  //_firebaseAuth.currentUser: Firebase에 currentUser가 있는지? 유무로 로그인 되었는지 확인함
+  // bool isLoggedIn() {
+  //   return (_firebaseAuth.currentUser != null);
+  // }
+  Stream<User?> authStateChange() => _firebaseAuth.authStateChanges();
+
+  Future<void> createUser(String email, String password) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    //.createUserWithEmailAndPassword(): 유저 이메일/비밀번호로 로그인 인증을 생성함
+  }
+
+  Future<void> userSignOut() async {
+    await _firebaseAuth.signOut();
+    //.signOut() 로그아웃하는 메서드
+  }
+
+  Future<void> userSignIn(String email, String password) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    //.signInWithEmailAndPassword: firebase 이메일,비밀번호로 로그인
+  }
 }
 
-//ViewModel
-class UserViewModel extends AsyncNotifier<User> {
-  //비동기적으로 Notifier를 사용하기위해 AsyncNotifier을 상속
+final authRepoProvider = Provider((ref) => AuthRepository());
+//AuthRepository() 를 expose 하기 위해 만들어진 Provider
+final authStreamProvider =
+    StreamProvider((ref) => ref.read(authRepoProvider).authStateChange());
+//AuthRepository()의 _firebaseAuth.authStateChanges() 로
+//sign-in 이나 sign-out의 실시간 변화를 expose하기 위해 만들어진 Provider
+
+////ViewModel/////////////////////////////////////////////////////////////////////////
+class LoginViewModel extends AsyncNotifier<void> {
+  late final AuthRepository _authRepository;
+
   @override
-  FutureOr<User> build() async {
-    final user = User(name: "name", autoLogin: false);
-    return user;
-    //FutureOr로 User 초기화
+  FutureOr<void> build() async {
+    _authRepository = ref.read(authRepoProvider);
+    //authRepoProvider로 AuthRepository()가 가진 것들을 _authRepository에 넣음
   }
 
-  Future<User> getUser() async {
-    final user =
-        User(name: state.value!.name, autoLogin: state.value!.autoLogin);
-    return user;
-    //User get 메소드
-  }
-
-  Future<void> setUserName(value) async {
+  Future<void> logIn(
+      String email, String password, BuildContext context) async {
     state = const AsyncValue.loading();
-    //state를 로딩상태로 만들어줌
-    await Future.delayed(const Duration(seconds: 2));
-    //Future.delayred로 강제로 2초간 딜레이를 줌(Loading되는 예시를 구현하기 위함)
-    state =
-        AsyncValue.data(User(name: value, autoLogin: state.value!.autoLogin));
-    //state에 data를 넣어줌 (로딩완료) set 매소드
+    //로딩 상태로 들어감
+    state = await AsyncValue.guard(
+        //guard: 에러발생과 데이터 처리결과 상태를 상황에 맞게 state에 넣어줌
+        () async => await _authRepository.userSignIn(email, password));
+    if (state.hasError) {
+      final snackBar = SnackBar(
+          content: Text(
+              (state.error as FirebaseException).message ?? "알수없는 오류입니다."));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      return;
+    }
   }
 
-  Future<void> setUserAutoLogin(value) async {
-    state = AsyncValue.data(User(name: state.value!.name, autoLogin: value));
+  Future<void> logOut() async {
+    await ref.read(authRepoProvider).userSignOut();
+  }
+
+  Future<void> createUsers(String email, String password) async {}
+
+  String getEmail() {
+    String email = _authRepository.user?.email ?? "로그인 해주세요.";
+    return email;
   }
 }
 
-final userProvider =
-    AsyncNotifierProvider<UserViewModel, User>(() => UserViewModel());
-//AsyncNotifierProvider 생성
+final userStateProvider = StateProvider((ref) => {});
+final userProvider = AsyncNotifierProvider<LoginViewModel, void>(
+  () => LoginViewModel(),
+);
 
-//View
-class UserViewPage extends ConsumerStatefulWidget {
-  const UserViewPage({super.key});
+////View////////////////////////////////////////////////////////////////////
+class LoginPage extends ConsumerStatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  UserViewPageState createState() => UserViewPageState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginPageState();
 }
 
-class UserViewPageState extends ConsumerState<UserViewPage> {
-  final TextEditingController _editingController = TextEditingController();
-  //텍스트 입력창에 입력된 Text를 이용하기 위해 컨트롤러 생성
+class _LoginPageState extends ConsumerState<LoginPage> {
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+
+  Future<void> _onTapLogin() async {
+    if (_emailTextEditingController.text.isEmpty ||
+        _passwordTextEditingController.text.isEmpty) return;
+
+    ref.read(userProvider.notifier).logIn(_emailTextEditingController.text,
+        _passwordTextEditingController.text, context);
+    setState(() {});
+  }
+
+  void _onTapLogout() {
+    ref.read(userProvider.notifier).logOut();
+    setState(() {});
+  }
+
+  void _onTapCreateUser() {
+    ref.read(userProvider.notifier).createUsers(
+        _emailTextEditingController.text, _passwordTextEditingController.text);
+    setState(() {});
+  }
 
   @override
   void initState() {
-    _editingController.addListener(() {});
-    //컨트롤러 초기화
+    _emailTextEditingController.addListener(() {});
+    _passwordTextEditingController.addListener(() {});
     super.initState();
   }
 
   @override
   void dispose() {
-    _editingController.dispose();
+    _emailTextEditingController.dispose();
+    _passwordTextEditingController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-            color: Colors.amber.shade800,
-            height: 300,
-            width: 300,
-            child: ref.watch(userProvider).when(
-                  //AsyncNotifierProvider 사용으로 when 기능 작성
-                  loading: () => const CircularProgressIndicator(),
-                  //loading: state가 로딩 상태일 때 보여줄 위젯을 작성하여 return함
-
-                  error: (error, stackTrace) => Text("$error"),
-                  //error: state가 로딩 상태일 때 보여줄 위젯을 작성하여 return함
-
-                  data: (data) => Column(
-                    //data: state가 데이터를 받았을 때 보여줄 위젯을 작성하여 return함
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        ref.watch(userProvider).value!.name,
-                        style: const TextStyle(fontSize: 40),
-                      ),
-                      TextField(
-                        controller: _editingController,
-                        cursorWidth: 2,
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                      SwitchListTile.adaptive(
-                        title: const Text("autoLogin"),
-                        value: ref.watch(userProvider).value!.autoLogin,
-                        onChanged: (value) => ref
-                            .read(userProvider.notifier)
-                            .setUserAutoLogin(value),
-                      ),
-                      CupertinoButton(
-                          child: const Text("이름 변경"),
-                          onPressed: () => ref
-                              .read(userProvider.notifier)
-                              .setUserName(_editingController.text))
-                    ],
-                  ),
-                )),
+        body: Form(
+            child: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ref.watch(authRepoProvider).isLoggedIn
+              ? Icon(
+                  Icons.gpp_good_sharp,
+                  size: 100,
+                  color: Colors.green.shade200,
+                )
+              : const Icon(
+                  Icons.supervised_user_circle,
+                  color: Colors.white,
+                  size: 100,
+                ),
+          Text(
+            ref.read(userProvider.notifier).getEmail(),
+            style: TextStyle(
+                color: Colors.amber.shade600,
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+          ),
+          TextFormField(
+            textAlign: TextAlign.center,
+            controller: _emailTextEditingController,
+            keyboardType: TextInputType.emailAddress,
+            validator: (value) {
+              if (value != null || value != "") {
+                return "check Your Email";
+              } else {
+                return null;
+              }
+            },
+          ),
+          TextFormField(
+            textAlign: TextAlign.center,
+            controller: _passwordTextEditingController,
+            keyboardType: TextInputType.number,
+            validator: (value) {
+              if (value != null || value != "") {
+                return "It is Not Safe";
+              } else {
+                return null;
+              }
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CupertinoButton(
+                  disabledColor: Colors.grey,
+                  color: Colors.green.shade500,
+                  onPressed: _onTapLogin,
+                  child: const Text("로그인"),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: CupertinoButton(
+                  disabledColor: Colors.grey,
+                  color: Colors.red.shade500,
+                  onPressed: _onTapLogout,
+                  child: const Text("로그아웃"),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CupertinoButton(
+              disabledColor: Colors.grey,
+              color: Colors.blue.shade300,
+              onPressed: _onTapCreateUser,
+              child: const Text("회원으로 가입하기"),
+            ),
+          ),
+        ],
       ),
-    );
+    )));
   }
 }
--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -42,7 +42,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => const UserViewPage(),
+                              builder: (context) => const LoginPage(),
                             ));
                       },
                       icon: const FaIcon(FontAwesomeIcons.bell)),
